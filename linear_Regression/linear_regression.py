a4db27f85e42e9cae50792b5e5b250d9f867a7a4
christo.lagali <christo.lagali@gmail.com>
2019-06-10 23:03:57 -0700

    Initial push with Data cleaning Methods

--- linear_Regression/linear_regression.py
+++ linear_Regression/linear_regression.py
@@ -1,7 +1,36 @@
 import sys
 import sparkconnect
+from pyspark.sql import Row
+
+# Remove header from the imported flat files
+def excludeHeader(autoRDD):
+
+    header = autoRDD.first()
+
+    autoRows = autoRDD.filter(lambda x:x!=header)
+
+    return autoRows
 
 
+# clean Data Method
+def cleanData(autoStr):
+
+    global avgHP
+    autoLst = autoStr.split(',')
+
+    if autoStr[3] == '?':
+        hpValue = avgHP
+    else:
+        hpValue = autoStr[3]
+    
+    return Row(MPG=float(autoLst[0]),CYLINDERS=float(autoLst[1]),DISPLACEMENT=float(autoLst[2]),HORSEPOWER=float(hpValue),WEIGHT=float(autoLst[4]),ACCELERATION=float(autoLst[5]),MODELYEAR=float(autoLst[6]),NAME=autoLst[7])
+
+
+
+
+##########################################################################################################################
+# Main Program
+##########################################################################################################################
 try:
 
     spContext = sparkconnect.getContext()
@@ -12,6 +41,18 @@ try:
 
     #print(autoData.take(5))
 
+    # Broadcast variable
+    avgHP = spContext.broadcast(80)
+
+    # cleaned RDD
+    autoRDD = excludeHeader(autoData)
+
+    autoCleanRDD = autoRDD.map(cleanData)
+
+    print(autoCleanRDD.take(5))
+
+
+
 
 except ImportError as ie:
     print(ie)
@@ -19,14 +60,5 @@ except ImportError as ie:
 except Exception as e:
     print(e)
 
-
-# Remove header from the imported flat files
-def excludeHeader(autoRDD):
-
-    header = autoRDD.first()
-
-    autoRows = autoRDD.filter(lambda x:x!=header)
-
-    return autoRows
 
 
